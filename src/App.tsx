b1d0f75a5a93dfdb02d0ad7c01b963fc0bb661d0
Polyane Tuag <pollyportugues@gmail.com>
2025-04-07 18:11:05 -0300

    feat: add header and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 
 
 import './App.css'
-import Home from './components/Home/Home'
+import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
       <Home />
       
     </>
