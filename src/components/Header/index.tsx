a33059b291d94f3e2c165f99d249839aca705c75
Polyane Tuag <pollyportugues@gmail.com>
2025-06-22 10:20:31 -0300

    style: simplify Header component by removing unused elements and classes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,24 +2,7 @@ import "./styles.css";
 
 function Header() {
   return (
-    <header className="header-container">
-      <div>
-        {/* <h2>Logo</h2> */}
-        <p>Universe</p>
-      </div>
-      {/* <img src="" alt="Logo" />
-      <h2>Universe</h2> */}
-      <nav>
-        <div className="nav-links">
-          <a href="#about">Sobre</a>
-          <a href="#explore">Explore</a>
-        </div>
-      </nav>
-      <div className="search-bar">
-        <input type="text" placeholder="Pesquisar..." />
-        <button type="submit">Buscar</button>
-      </div>
-    </header>
+    <header className="flex justify-center items-center p-4 text-white"></header>
   );
 }
 
