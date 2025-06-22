29b5ac055059fdffd69e7c4ebb8df0e7f92069bc
Polyane Tuag <pollyportugues@gmail.com>
2025-06-22 10:21:02 -0300

    style: format App component for improved readability and consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
-
-
-import './App.css'
-import Header from './components/Header'
-import Home from './components/Home'
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-
   return (
-    <>
+    <div className="mx-auto max-w-4xl p-5">
       <Header />
       <Home />
-      
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
