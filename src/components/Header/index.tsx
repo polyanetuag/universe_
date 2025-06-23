8316b0b6d6d5b25a63a4f70c8091cbc44540126a
Polyane Tuag <pollyportugues@gmail.com>
2025-06-23 19:34:32 -0300

    style: enhance Header component with title and description for improved user engagement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,14 @@ import "./styles.css";
 
 function Header() {
   return (
-    <header className="flex justify-center items-center p-4 text-white"></header>
+    <header className="flex flex-col justify-center items-center mx-auto p-4 text-white">
+      <h1 className="text-3xl font-bold">
+        🛰️NASA Astronomy Picture of the Day
+      </h1>
+      <p className="text-gray-400 ml-4">
+        Explore the wonders of the universe with NASA's daily image.
+      </p>
+    </header>
   );
 }
 
