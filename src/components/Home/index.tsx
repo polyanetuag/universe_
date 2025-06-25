6c0cf648f4c457ed9de6ca13ef3aa1c9e18489f8
Polyane Tuag <pollyportugues@gmail.com>
2025-06-25 19:05:12 -0300

    style: reorganize layout of Home component for improved structure and readability

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -31,21 +31,34 @@ function Home() {
 
   return (
     <>
-      <div className="grid p-10 gap-2 grid-cols-2 shadow-black shadow-lg rounded-lg ">
-        <div className="col-span-2 flex justify-center mb-6">
-          <h2 className="text-2xl text-white flex justify-center mb-6">
-            {dadosExibir?.title}
-          </h2>
+      <div className="flex flex-row items-center justify-center">
+        <div>
+          <button className="w-26 flex  bg-gray-900 text-gray-400 px-4 py-2 mr-4 rounded-lg shadow-md hover:bg-gray-800 text-sm cursor-pointer">
+            ⪡ Previous
+          </button>
         </div>
-        <div className="col-span-1 flex justify-center ">
-          <p className="text-gray-400">{dadosExibir?.explanation}</p>
+        <div className="grid p-10 gap-2 grid-cols-2 shadow-black shadow-lg rounded-lg ">
+          <div className="col-span-2 flex justify-center mb-6">
+            <h2 className="text-2xl text-white flex justify-center mb-6">
+              {dadosExibir?.title}
+            </h2>
+          </div>
+          <div className="col-span-1 flex justify-center ">
+            <p className="text-gray-400">{dadosExibir?.explanation}</p>
+          </div>
+          <div className="col-span-1 flex justify-center items-center">
+            <img
+              className="w-full h-full object-cover rounded-lg shadow-lg"
+              src={dadosExibir?.url}
+              alt=""
+            />
+          </div>
         </div>
-        <div className="col-span-1 flex justify-center items-center">
-          <img
-            className="w-full h-full object-cover rounded-lg shadow-lg"
-            src={dadosExibir?.url}
-            alt=""
-          />
+
+        <div>
+          <button className="min-w-22 flex bg-gray-900 text-gray-400 px-4 py-2 ml-4 rounded-lg shadow-md hover:bg-gray-800 text-sm cursor-pointer">
+            Next ⪢
+          </button>
         </div>
       </div>
       <footer className="flex justify-center items-center text-gray-600 p-2 mt-10">
